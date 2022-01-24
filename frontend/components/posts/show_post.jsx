fae92bd4304fb76d04889e6eaeeda01e47816c07
MalachiCoberley <malachicoberley@gmail.com>
2022-01-24 11:32:20 -0700

    Connect Follow button on post display. Change avatar letter to placeholder image

--- frontend/components/posts/show_post.jsx
+++ frontend/components/posts/show_post.jsx
@@ -5,6 +5,21 @@ class ShowPost extends React.Component {
   constructor(props) {
     super(props);
     this.state = this.props.post;
+
+    this.followUser = this.followUser.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.getUser(this.state.author_id).then(() => this.setState({loading: false}))
+    this.props.getFollows(this.state.author_id)
+  }
+
+  followUser(e) {
+    let follow = {
+      user_id: this.state.author_id,
+      follower: this.props.session.id
+    }
+    this.props.createFollow(follow)
   }
 
   render() {
@@ -19,11 +34,17 @@ class ShowPost extends React.Component {
             <button onClick={() => this.props.deletePost(this.state.id)}>Delete Photo</button>
           </div>
           <div className='show-author-information'>
-            <div className='show-author-picture'><Link to={`/user/${this.state.author_id}`}>A</Link></div>
+            <div className='show-author-picture'>
+              <Link to={`/user/${this.state.author_id}`}>
+                <img src="https://pacdn.500px.org/userpic.png" alt='profile picture'>
+                </img>
+              </Link>
+            </div>
             <div className='show-title-follow'>
               <h3>{this.state.title}</h3>
               <div className='show-author-name-follow'>
-              <p>by {this.state.author_id} • </p><button>Follow</button>
+              <p>by {this.state.author_id} • </p>
+              <button onClick={e => this.followUser(e)}>Follow</button>
               </div>
             </div>
           </div>
--- frontend/components/posts/show_post_container.js
+++ frontend/components/posts/show_post_container.js
@@ -1,16 +1,22 @@
 import { connect } from 'react-redux';
 import ShowPost from './show_post';
 import { fetchPost, deletePost } from '../../actions/post_actions';
+import { createFollow, getFollows } from '../../actions/follow_actions';
+import { getUser } from '../../actions/user_actions';
 
 
 const mapStateToProps = (state, ownProps) => ({
   post: state.entities.posts[ownProps.match.params.postId],
-  formType: 'Update Post'
+  loading: true,
+  session: state.session
 });
 
 const mapDispatchToProps = dispatch => ({
 fetchPost: postId => dispatch(fetchPost(postId)),
-deletePost: postId => dispatch(deletePost(postId))
+deletePost: postId => dispatch(deletePost(postId)),
+getUser: userId => dispatch(getUser(userId)),
+createFollow: follow => dispatch(createFollow(follow)),
+getFollows: userId => dispatch(getFollows(userId))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ShowPost);
